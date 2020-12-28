f2a82494d8b820c73c46a7a6a58aeabca2d9d9c2
Jamikon <43286344+Jamikon@users.noreply.github.com>
2020-12-28 12:12:53 +1100

    imported linkstations (#2)
    ignore testing as no test criteria added yet

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
+import LinkStations from "./LinkStations/LinkStations"
 import PredefinedPoints from "./PredefinedPoints/PredefinedPoints";
 
 function main(){
-
-    // load LinkStations
 
     // run algorithm on arrays, return results in array?
 
