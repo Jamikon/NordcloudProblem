4f85ee61bf50fa6f5dcdca98ce9d602119ce16f8
Jamikon <wjamikon@gmail.com>
2021-01-03 16:56:46 +1100

    wip

--- src/main.js
+++ src/main.js
@@ -1,9 +1,24 @@
 import LinkStations from "./LinkStations/LinkStations"
 import PredefinedPoints from "./PredefinedPoints/PredefinedPoints";
+import getLinkStationPower from "./LinkStations/getLinkStationPower";
+
+function processData(Points, LinkStations){
+
+
+
+    console.log(Points);
+    console.log(LinkStations);
+}
 
 function main(){
 
-    // run algorithm on arrays, return results in array?
+    console.log(getLinkStationPower(PredefinedPoints[0], LinkStations[1]));
+
+    for (let Point of PredefinedPoints){
+        // iterate over all points... checking all LinkStations for each Point
+
+        console.log(getLinkStationPower(Point, LinkStations[0]));
+    }
 
     // print results using logger
 
