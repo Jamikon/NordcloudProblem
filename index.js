c683dc0397f402518c7e4c54495b8a44a31b06cc
Jamikon <wjamikon@gmail.com>
2021-01-11 00:39:32 +1100

    renamed lambda entry ti 'index.js' to comply with file-name.method as per lambda docs

---
+++ index.js
@@ -0,0 +1,14 @@
+import main from "./src/main";
+
+exports.handler = async (event) => {
+
+    let Points = event.Points;
+    let LinkStations = event.LinkStations;
+
+    let output = await main(Points, LinkStations);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(output),
+    };
+};
